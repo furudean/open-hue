59b948b399782f02a11342dc990765cc79f4d91d
uhm-jade <uhm.jade@gmail.com>
2018-10-20 16:18:16 -0700

    attempt switch to chroma.scale

--- app/components/game/gradient.service.js
+++ app/components/game/gradient.service.js
@@ -13,13 +13,11 @@ angular.module('game')
       return start + (end - start) * t
     }
 
-    const biLerp = (lerpFn) =>
-      (x0y0, x1y0, x0y1, x1y1, dx, dy) =>
-        lerpFn(
-          lerpFn(x0y0, x1y0, dx), // top
-          lerpFn(x0y1, x1y1, dx), // bottom
-          dy
-        );
+    function biLerp(x0y0, x1y0, x0y1, x1y1, dx, dy) {
+      const top = chroma.scale([x0y0, x1y0])(dx);
+      const bottom = chroma.scale([x0y1, x1y1])(dx);
+      return chroma.scale([top, bottom])(dy);
+    }
 
     const toCompositeColor = (corners) => Object.values(corners)
       .map((color) => chroma(color).lch());
@@ -35,19 +33,19 @@ angular.module('game')
       const width = matrix[0].length;
       const height = matrix.length;
 
-      const [tl, tr, bl, br] = toCompositeColor(corners);
+      const [tl, tr, bl, br] = Object.values(corners);
 
       function interpolate(cell, x, y) {
         const dx = x/(width - 1);
         const dy = y/(height - 1);
 
         $log.debug({x, y, dx, dy});
+        // const l = biLerp(lerp)(tl[0], tr[0], bl[0], br[0], dx, dy);
+        // const c = biLerp(lerp)(tl[1], tr[1], bl[1], br[1], dx, dy);
+        // const h = biLerp(lerpAngle)(tl[2], tr[2], bl[2], br[2], dx, dy);
 
-        const l = biLerp(lerp)(tl[0], tr[0], bl[0], br[0], dx, dy);
-        const c = biLerp(lerp)(tl[1], tr[1], bl[1], br[1], dx, dy);
-        const h = biLerp(lerpAngle)(tl[2], tr[2], bl[2], br[2], dx, dy);
-
-        const combined = chroma(l, c, h, 'lch').toString();
+        // const combined = chroma(l, c, h, 'lch').toString();
+        const combined = biLerp(tl, tr, bl, br, dx, dy);
 
         cell.color = combined;
       }
