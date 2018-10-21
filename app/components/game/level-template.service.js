ee721adb048d0e53a98d21b094e1f9e5b16fe262
Cassidy Bandy <cass@whimz.ee>
2018-10-21 02:28:10 +0200

    some design stuff

--- app/components/game/level-template.service.js
+++ app/components/game/level-template.service.js
@@ -2,13 +2,26 @@ angular.module('game')
   .factory('levelTemplateService', () => {
     const templates = [
       {
-        name: 'the big boy',
-        matrix: 'xooooox\n'.repeat(7),
+        name: 'first steps',
+        matrix:
+          'xxx\n' +
+          'xox\n'.repeat(8) +
+          'xxx\n',
         corners: {
-          tl: '#FF8711',
-          tr: '#FF8711',
-          bl: '#FF3F11',
-          br: '#FF3F11',
+          tl: '#ffc711',
+          tr: '#ffc711',
+          bl: '#ff760d',
+          br: '#ff760d',
+        },
+      },
+      {
+        name: 'the big boy',
+        matrix: 'xooox\n'.repeat(5),
+        corners: {
+          tl: '#3F0039',
+          tr: '#820046',
+          bl: '#B10D45',
+          br: '#E05917',
         },
       },
     ];
