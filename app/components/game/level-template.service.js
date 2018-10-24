ba23da6a32499aa8fbe43aa5f7169120fb5d9b3e
Cassidy Bandy <cass@whimz.ee>
2018-10-24 19:58:16 +0200

    update levels

--- app/components/game/level-template.service.js
+++ app/components/game/level-template.service.js
@@ -15,13 +15,28 @@ angular.module('game')
         },
       },
       {
-        name: 'the big boy',
+        name: 'oyasumi',
         matrix: 'x4ox\n'.repeat(6),
         corners: {
           topLeft: '#3F0039',
           topRight: '#820046',
           bottomLeft: '#B10D45',
           bottomRight: '#E05917',
+        },
+      },
+      {
+        name: 'lake',
+        matrix:
+          '6x\n' +
+          'x4ox\n'.repeat(2) +
+          '6x\n'.repeat(2) +
+          'x4ox\n'.repeat(2) +
+          '6x',
+        corners: {
+          topLeft: '#5B9279',
+          topRight: '#5AB1BB',
+          bottomLeft: '#8FCB9B',
+          bottomRight: '#9ee2ea',
         },
       },
     ];
