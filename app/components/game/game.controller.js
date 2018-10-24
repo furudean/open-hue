fa011bc258e24a4ca73b70aabdd12a4cc294c272
Cassidy Bandy <cass@whimz.ee>
2018-10-24 20:18:44 +0200

    cute little entry animation

--- app/components/game/game.controller.js
+++ app/components/game/game.controller.js
@@ -4,6 +4,9 @@ angular.module('game')
     const {Board} = boardService;
     let board;
 
+    const sleep = async (ms) => new Promise((resolve) => $timeout(resolve, ms));
+
+
     vm.onTileMoved = function(event) {
       const isWin = board.isWin();
       if (isWin) {
@@ -14,14 +17,17 @@ angular.module('game')
       }
     };
 
-    function init() {
+    async function init() {
       const template = levelTemplateService.templates[$stateParams.id - 1];
-      board = vm.board = new Board(template);
-      $timeout(async () => {
-        await board.setHiddenAll(true);
-        board.shuffle();
-        board.setHiddenAll(false);
-      }, 2000);
+      vm.board = board = new Board(template);
+      board.setHiddenAll(true, 0, true);
+
+      await sleep(); // run an angular cycle to make sure the board is loaded
+      await board.setHiddenAll(false, 20, true);
+      await sleep(2000);
+      await board.setHiddenAll(true);
+      board.shuffle();
+      board.setHiddenAll(false);
     }
 
     init();
